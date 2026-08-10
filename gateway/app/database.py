ebb88d023c7ae611511c0341d8ce493d1ad93724
Matvey <doskirin@ya.ru>
2026-08-10 12:09:17 +0300

    fix: fixendpoin

--- gateway/app/database.py
+++ gateway/app/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
-DB_URL = "postgresql+asyncpg://admin:root@localhost:5432/smartbank"
-engine = create_async_engine(DB_URL)
+DATABASE_URL = "postgresql+asyncpg://admin:root@db:5432/smartbank"
+engine = create_async_engine(DATABASE_URL)
 local_session = async_sessionmaker(engine)
--- gateway/app/main.py
+++ gateway/app/main.py
@@ -1,19 +1,25 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from sqlalchemy import text
 
+from app.api import router as api_router
 from app.database import engine
 from app.models import Base
 
 
 @asynccontextmanager
-async def lifespawn(app: FastAPI):
+async def lifespan(app: FastAPI):
     async with engine.begin() as conn:
+        await conn.run_sync(
+            lambda sync_conn: sync_conn.execute(text("CREATE EXTENSION IF NOT EXISTS vector;"))
+        )
         await conn.run_sync(Base.metadata.create_all)
     yield
 
 
-app = FastAPI(title="SmartBank API GateWay", lifespawn=lifespawn)
+app = FastAPI(title="SmartBank API GateWay", lifespan=lifespan)
+app.include_router(api_router)
 
 
 @app.get("/")
