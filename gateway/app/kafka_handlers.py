18645590eac2744f1d3c07c30e3b3e6f923cc763
Matvey <doskirin@ya.ru>
2026-08-22 19:19:54 +0300

    feat: add kafka models and subcriber

--- gateway/app/kafka_handlers.py
+++ gateway/app/kafka_handlers.py
@@ -1,11 +1,11 @@
 from uuid import UUID
 
-from broker import kafka_router
 from fastapi import Depends
 from pydantic import BaseModel, Field
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from .broker import kafka_router
 from .database import get_db
 from .models import TaskRecord
 
@@ -20,12 +20,10 @@ class WorkerResponses(BaseModel):
 async def get_task_info(m: WorkerResponses, db: AsyncSession = Depends(get_db)):
     query = select(TaskRecord).where(TaskRecord.task_id == m.task_id)
     result_db = await db.execute(query)
-    result = m.result
     task = result_db.scalar_one_or_none()
-    status = m.status
 
     if task is None:
         return
 
-    task.status = status  # type: ignore
-    task.result = result  # type: ignore
+    task.status = m.status  # type: ignore
+    task.result = m.result  # type: ignore
